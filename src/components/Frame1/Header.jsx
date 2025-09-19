3f62f5b3dd457bd26b877b37dcfcf166a7b3d352
umar-redtooth <omeridrees147@gmail.com>
2025-09-19 09:40:51 +0500

    header

--- src/components/Frame1/Header.jsx
+++ src/components/Frame1/Header.jsx
@@ -5,16 +5,16 @@ export default function Header() {
         <h1 className="absolute font-[Poppins] font-bold text-[32px] tracking-[4%] z-10 top-[5px] left-[20px]">Skilline</h1>
         <img src="/logo.png" alt="Skilline" className="absolute z-0 w-[83px] h-[83px] top-[-12px]"/>
       </div>
-      
+
       <div className="flex gap-11">
         
         <ul className="list-non flex gap-11 font-[Poppins] text-[#252641] font-normal text-[22px] tracking-[2%] mt-3.5">
-        <li className="hover:text-[#F48C06]">Home</li>
-        <li className="hover:text-[#F48C06]">Careers</li>
-        <li className="hover:text-[#F48C06]">Blog</li>
-        <li className="hover:text-[#F48C06]">About US</li>
+          <li className="hover:text-[#F48C06]">Home</li>
+          <li className="hover:text-[#F48C06]">Careers</li>
+          <li className="hover:text-[#F48C06]">Blog</li>
+          <li className="hover:text-[#F48C06]">About US</li>
         </ul>
-        
+
         <div className="flex gap-6 font-[Poppins] font-normal text-[22px]">
         <button className="bg-white hover:bg-black hover:text-white w-[160px] h-[60px] rounded-[80px]">Login</button>
         <button className="bg-[#F48C06] hover:bg-black hover:text-[#F48C06] w-[160px] h-[60px] rounded-[80px]">Signup</button>
